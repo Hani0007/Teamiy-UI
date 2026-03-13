188c363684ff118d8821654f5de8ac308703a438
jimmymilton704-star <jimmymilton704@gmail.com>
2026-03-13 14:16:09 +0500

    Added Lang Files

--- resources/views/layouts/nav_search_scripts.blade.php
+++ resources/views/layouts/nav_search_scripts.blade.php
@@ -3,90 +3,96 @@
 {{--<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>--}}
 
 <script>
-  $(document).ready(function () {
-    $('#nav-search').val('');
-    let currentLI = -1; // start from -1 because no item is highlighted initially
-    const keydown = 38;
-    const keyup = 40;
-    const keyEnter = 13;
+    $(document).ready(function () {
+        $('#nav-search').val('');
+        let currentLI = 0;
+        const keydown = 38;
+        const keyup = 40;
+        const keyEnter = 13;
 
-    function showDropdown() {
-        $('.card-admin-search').show();
-    }
+        $("#nav-search").on("keyup", function (e) {
+            e.preventDefault();
+            let keycode = e.keyCode;
 
-    function hideDropdown() {
-        $('.card-admin-search').hide();
-        currentLI = -1;
-    }
+            if(keycode !== keydown && keycode !== keyup){
+                let linksArr = {};
+                $('.sidebar-menu li a').each(function (i) {
+                    let href = $(this).data('href');
+                    if(href !=='#'){
+                        let name = $(this).text().toLowerCase();
+                        linksArr[name] = $(this).data('href');
+                    }
+                });
 
-    $("#nav-search").on("keyup", function (e) {
-        let keycode = e.keyCode;
-        const listContainer = $('#nav-search-listing');
-        const listItems = listContainer.find('li');
+                let linkKeys = Object.keys(linksArr)
+                let searchQuery = $('#nav-search').val();
+                if (searchQuery.length >= 0) {
+                    $('#nav-search-listing li').remove();
+                    if (searchQuery) {
+                        for (i = 0; i < linkKeys.length; i++) {
+                            let hreflocation = linksArr[linkKeys[i]];
 
-        // handle arrow keys and enter
-        if (keycode === keydown || keycode === keyup || keycode === keyEnter) {
-            if (listItems.length === 0) return;
-            listItems.removeClass('highlight');
+                            if (linkKeys[i].toLowerCase().indexOf(searchQuery) > -1) {
+                                $('#nav-search-listing').
+                                append( '<li id="nav-menu" class="nav-listing list-group-item">' +
+                                    '<a href=" '+hreflocation+' ">'+linkKeys[i]+'</a></li>');
 
-            if (keycode === keydown) {
-                currentLI = currentLI > 0 ? currentLI - 1 : 0;
-            } else if (keycode === keyup) {
-                currentLI = currentLI < listItems.length - 1 ? currentLI + 1 : listItems.length - 1;
-            } else if (keycode === keyEnter) {
-                const url = listItems.eq(currentLI).find('a').attr('href');
-                if (url) window.location.href = url;
+                            }
+                        }
+                    }
+                }
             }
 
-            if (currentLI >= 0) listItems.eq(currentLI).addClass('highlight');
-            return;
-        }
+            //for up down with arrow key highlight/select
 
-        // normal typing search
-        let linksArr = {};
-        $('.sidebar-menu li a').each(function () {
-            let href = $(this).data('href');
-            if (href && href !== '#') {
-                let name = $(this).text().toLowerCase();
-                linksArr[name] = href;
+            let listItems = document.querySelectorAll("#nav-search-listing li");
+            listItems[currentLI].classList.add("highlight");
+            if(keycode == keydown) {
+                listItems[currentLI].classList.remove("highlight");
+                currentLI = currentLI > 0 ? --currentLI : 0;
+                console.log(currentLI);
+                listItems[currentLI].classList.add("highlight");
+            }
+
+            if(keycode == keyup){
+                listItems[currentLI].classList.remove("highlight");
+                currentLI = currentLI < listItems.length - 1 ? ++currentLI : listItems.length - 1;
+                listItems[currentLI].classList.add("highlight");
+            }
+
+            //redirect to highlighted url on enter key press
+            if(keycode == keyEnter){
+                let url = $('.highlight').find('a').attr('href');
+                window.location.href = url;
+            }
+
+        });
+
+        // ctrl + Q command for focusing in admin search field
+        window.addEventListener("keydown",function (e) {
+            if (e.ctrlKey && e.keyCode === 81){
+                if($('#nav-search').is(":focus")) {
+                    return true;
+                } else {
+                    e.preventDefault();
+                    $('#nav-search').focus();
+                }
             }
         });
 
-        const searchQuery = $(this).val().toLowerCase();
-        listContainer.empty();
-
-        if (searchQuery.length > 0) {
-            for (let name in linksArr) {
-                if (name.indexOf(searchQuery) !== -1) {
-                    listContainer.append('<li class="nav-listing list-group-item"><a href="'+linksArr[name]+'">'+name+'</a></li>');
-                }
+        // toggle hide and show based on input field focus
+        $(document).click(function(evt) {
+            if ($(evt.target).closest('#admin-search-menu').length === 0) {
+                $('.card-admin-search').hide();
             }
-            showDropdown();
-        } else {
-            hideDropdown();
-        }
+        });
 
-        currentLI = -1;
-    });
-
-    // hide dropdown when clicking outside
-    $(document).click(function (evt) {
-        if (!$(evt.target).closest('.search-form').length) {
-            hideDropdown();
-        }
-    });
-
-    // focusin shows dropdown
-    $('#nav-search').focusin(showDropdown);
-
-    // ctrl + Q to focus
-    window.addEventListener("keydown", function (e) {
-        if (e.ctrlKey && e.keyCode === 81) {
+        $('#admin-search-menu').focusin(function(e){
             e.preventDefault();
-            $('#nav-search').focus();
-        }
+            $('.card-admin-search').show();
+        });
+
     });
-});
 
 
 </script>
